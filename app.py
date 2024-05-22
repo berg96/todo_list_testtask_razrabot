832b19613f7cce16b20d63a47ed02e7230429f0b
Artem Kulikov <chigar2010@yandex.ru>
2024-05-22 23:03:24 +0200

    add

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
+import os
 from datetime import datetime
 
-from flask import Flask, request, abort, jsonify
+from dotenv import load_dotenv
+from flask import Flask, request, abort, jsonify, flash
 from flask_restful import fields, marshal_with
 
 from models import Task, db
 
+load_dotenv()
+DB_NAME = os.getenv('MYSQL_DATABASE')
+DB_USER = os.getenv('MYSQL_USER')
+DB_PASSWORD = os.getenv('MYSQL_PASSWORD')
+DB_HOST = os.getenv('MYSQL_HOST')
+DB_CONTAINER_NAME = 'mysql_db'
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo_list.db'
-app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f'mysql://{DB_USER}:{DB_PASSWORD}@{DB_CONTAINER_NAME}:{DB_HOST}/{DB_NAME}'
+)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db.init_app(app)
 
 task_fields = {
@@ -36,6 +47,7 @@ def create_task():
     )
     db.session.add(task)
     db.session.commit()
+    flash('Задача создана!', 'success')
     return task, 201
 
 
@@ -55,6 +67,7 @@ def update_task(task_id):
     task.description = request.json.get('description', task.description)
     task.updated_at = datetime.now()
     db.session.commit()
+    flash('Задача обновлена!', 'success')
     return task
 
 
@@ -62,7 +75,8 @@ def update_task(task_id):
 def delete_task(task_id):
     db.session.delete(Task.query.get_or_404(task_id))
     db.session.commit()
-    return {'detail': 'Задача успешно удалена'}, 204
+    flash('Задача удалена!', 'success')
+    return jsonify({'detail': 'Задача успешно удалена'})
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -8,6 +8,6 @@ db = SQLAlchemy()
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
-    description = db.Column(db.String(), nullable=True)
+    description = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
     updated_at = db.Column(db.DateTime, nullable=True)
